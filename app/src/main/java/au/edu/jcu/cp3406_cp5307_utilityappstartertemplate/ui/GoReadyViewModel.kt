7f42e9adb7950b8151552c5a324d09a76d879f26
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2026-06-14 21:34:10 +0800

    Refactor the code to connect GoReadyViewModel to weather repository for state updates

--- app/src/main/java/au/edu/jcu/cp3406_cp5307_utilityappstartertemplate/ui/GoReadyViewModel.kt
+++ app/src/main/java/au/edu/jcu/cp3406_cp5307_utilityappstartertemplate/ui/GoReadyViewModel.kt
@@ -4,14 +4,24 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import au.edu.jcu.cp3406_cp5307_utilityappstartertemplate.data.WeatherRepository
 
 class GoReadyViewModel : ViewModel() {
 
-    var uiState by mutableStateOf(GoReadyUiState())
+    private val weatherRepository = WeatherRepository()
+
+    var uiState by mutableStateOf(
+        GoReadyUiState(
+            weather = weatherRepository.getWeather("Singapore")
+        )
+    )
         private set
 
     fun selectCity(city: String) {
-        uiState = uiState.copy(selectedCity = city)
+        uiState = uiState.copy(
+            selectedCity = city,
+            weather = weatherRepository.getWeather(city)
+        )
     }
 
     fun setUseFahrenheit(useFahrenheit: Boolean) {
@@ -27,6 +37,9 @@ class GoReadyViewModel : ViewModel() {
     }
 
     fun refreshAdvice() {
-        uiState = uiState.copy(refreshCount = uiState.refreshCount + 1)
+        uiState = uiState.copy(
+            weather = weatherRepository.getWeather(uiState.selectedCity),
+            refreshCount = uiState.refreshCount + 1
+        )
     }
 }
