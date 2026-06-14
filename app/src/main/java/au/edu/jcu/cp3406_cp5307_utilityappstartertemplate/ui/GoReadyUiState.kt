a986d8b2c69c6a3edb12472945019b9ae36e6e6d
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2026-06-14 21:28:48 +0800

    Refactor the code to add weather snapshot to GoReady UI state for main screen display

--- app/src/main/java/au/edu/jcu/cp3406_cp5307_utilityappstartertemplate/ui/GoReadyUiState.kt
+++ app/src/main/java/au/edu/jcu/cp3406_cp5307_utilityappstartertemplate/ui/GoReadyUiState.kt
@@ -1,7 +1,16 @@
 package au.edu.jcu.cp3406_cp5307_utilityappstartertemplate.ui
 
+import au.edu.jcu.cp3406_cp5307_utilityappstartertemplate.data.WeatherSnapshot
+
 data class GoReadyUiState(
     val selectedCity: String = "Singapore",
+    val weather: WeatherSnapshot = WeatherSnapshot(
+        city = "Singapore",
+        temperatureC = 30,
+        rainChance = 68,
+        uvIndex = 9,
+        windKmh = 14
+    ),
     val useFahrenheit: Boolean = false,
     val showDetails: Boolean = true,
     val detailedAdvice: Boolean = true,
